23b560bb4b1e450f027fe5c332129b456c386245
nanonate32 <league@iMac-2.attlocal.net>
2018-04-01 11:34:40 -0700

    WIP hangman! :)

--- src/other/Hangman.java
+++ src/other/Hangman.java
@@ -1,29 +1,89 @@
 package other;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Hangman {
+public class Hangman implements KeyListener{
 
 	JFrame frame = new JFrame("Hangman");
 	JPanel panel = new JPanel();
-	JTextField textField = new JTextField();
+	JLabel textLabel = new JLabel();
+	Stack<String> puzzles = new Stack<String>();
+	String word = "";
+	String hidden = "";
+	String solving = "";
 	
 	public static void main(String[] args) {
 	
 	Hangman hangman = new Hangman();
 	hangman.setup();
-	
+		
 	}
 	
 	void setup() {
 	
+	puzzles.add("librarian");
+	puzzles.add("justification");
+	puzzles.add("retaliation");
 	frame.setSize(200, 100);
-	panel.add(textField);
+	frame.addKeyListener(this);
+	word = puzzles.pop();
+	int length = word.length();
+	for (int i = 0; i < length; i++) {
+	hidden += "_";
+	hidden += " ";
+	}
+	textLabel.setText(hidden);
+	System.out.println(hidden);
+	panel.add(textLabel);
 	frame.add(panel);
-	frame.setVisible(true);
+	frame.setVisible(true);	
+	
+	}
+	
+	void createUI() {
+	
+	}
+	
+	void addPuzzles() {
+		
+		
 		
 	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	for (int i = 0; i < word.length(); i++) {
+		//char m = word.charAt(i);
+		if(word.charAt(i) == e.getKeyChar()) {
+		solving += word.charAt(i);
+		}else {
+		solving += "_";
+		}
+		solving += " ";
+	}
+	textLabel.setText(solving);
+	solving = "";
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
